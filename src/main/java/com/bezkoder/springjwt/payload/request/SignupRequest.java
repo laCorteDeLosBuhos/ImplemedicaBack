65b40020c32908db39aa5fe0c27e19e4d4753794
Santiago <OCEANS\omoutsiscomputo>
2020-09-06 14:56:25 -0500

    make it better

---
+++ src/main/java/com/bezkoder/springjwt/payload/request/SignupRequest.java
@@ -0,0 +1,77 @@
+package com.bezkoder.springjwt.payload.request;
+
+import java.util.Set;
+
+import javax.validation.constraints.*;
+ 
+public class SignupRequest {
+    @NotBlank
+    @Size(min = 3, max = 120)
+    private String username;
+ 
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+    
+    private Set<String> role;
+    
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+  
+    @NotBlank
+    @Size(max = 50)
+    private String Celular;
+    
+    @NotBlank
+    @Size(max = 120)
+    private String nombre;
+
+
+	public String getUsername() {
+        return username;
+    }
+ 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+ 
+    public String getEmail() {
+        return email;
+    }
+ 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+ 
+    public String getPassword() {
+        return password;
+    }
+ 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public Set<String> getRole() {
+      return this.role;
+    }
+    
+    public void setRole(Set<String> role) {
+      this.role = role;
+    }
+    public String getCelular() {
+		return Celular;
+	}
+
+	public void setCelular(String celular) {
+		Celular = celular;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
