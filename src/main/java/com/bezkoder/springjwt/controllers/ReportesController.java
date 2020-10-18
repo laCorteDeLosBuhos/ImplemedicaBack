6723f4adb1bc244fd15c646b8df0d436ef57f72e
Santiago <OCEANS\omoutsiscomputo>
2020-10-18 15:18:51 -0500

    Reportes por marca

--- src/main/java/com/bezkoder/springjwt/controllers/ReportesController.java
+++ src/main/java/com/bezkoder/springjwt/controllers/ReportesController.java
@@ -1,6 +1,8 @@
 package com.bezkoder.springjwt.controllers;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bezkoder.springjwt.models.DetallePedido;
+import com.bezkoder.springjwt.models.Pedidos;
 import com.bezkoder.springjwt.repository.ReportesRepository;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -22,6 +26,23 @@ public class ReportesController {
 		int[] is = new int[12];
 		for (int i = 0; i < 12; i++) {
 			is[i]=ciudadRepository.findbyyear(i+1, ano).size();
+		}
+		return ResponseEntity.ok(is);
+	}
+	@GetMapping("/getAllMarca/{ano}/{linea}")
+	public ResponseEntity<?> GetDatosMarca(@PathVariable int ano,@PathVariable String linea){
+		int[] is = new int[12];
+		for (int i = 0; i < 12; i++) {
+			int cantidad=0;
+			List<Pedidos> pedidos=ciudadRepository.findbyyear(i+1, ano);
+			for (Pedidos detalle:pedidos) {
+				for (DetallePedido detalles:detalle.getProductos()) {
+					if(detalles.getProducto().getLinea().startsWith(linea)) {
+						cantidad=cantidad+detalles.getCantidad();
+					}
+				}	
+			}
+			is[i]=cantidad;
 		}
 		return ResponseEntity.ok(is);
 	}
