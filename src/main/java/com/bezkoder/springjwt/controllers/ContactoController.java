baf93f492d23644d31efa60409397db91ee4621e
Santiago <OCEANS\omoutsiscomputo>
2020-11-03 23:05:09 -0500

    make it better

--- src/main/java/com/bezkoder/springjwt/controllers/ContactoController.java
+++ src/main/java/com/bezkoder/springjwt/controllers/ContactoController.java
@@ -38,8 +38,8 @@ public class ContactoController {
 		MimeMessageHelper helper = new MimeMessageHelper(msg, true);
 		helper.setTo("desarrollo2@lacortedelosbuhos.com");
 		helper.setFrom(Remitente);
-		Path path=Paths.get("index.html");
-		File directoryPath = new File("/app");
+		Path path=Paths.get("/app/src/index.html");
+		File directoryPath = new File("/app/src");
 	      //List of all files and directories
 	      String contents[] = directoryPath.list();
 	      System.out.println("List of files and directories in the specified directory:");
